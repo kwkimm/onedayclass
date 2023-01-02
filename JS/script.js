b4f554c7b5d57d7210f9f7f4d80e5561a50e72e0
kwkim <rusdnkim@gmail.com>
2023-01-02 16:38:06 +0900

    carousel0.1

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+
+let no = 1;
+const wrap = $('.wrap');
+
+$('.prev').on('click', function() {
+  
+  if(no > 1){
+    wrap.css('transform', `translateX(-${no-2}00vw)`);
+    no--;
+  }
+  
+});
+
+
+$('.next').on('click', function() {
+
+  if(no < 4) {
+    wrap.css('transform', `translateX(-${no}00vw)`);
+    no++;
+  }
+
+});
