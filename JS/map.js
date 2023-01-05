bab29a13b846b226f536db45da7126293948983a
kwkim <rusdnkim@gmail.com>
2023-01-05 10:31:20 +0900

    map05

--- JS/map.js
+++ JS/map.js
@@ -16,3 +16,28 @@ var marker = new kakao.maps.Marker({
 
 // 마커가 지도 위에 표시되도록 설정합니다
 marker.setMap(map);
+
+// 지도타입 컨트롤의 지도 또는 스카이뷰 버튼을 클릭하면 호출되어 지도타입을 바꾸는 함수입니다
+function setMapType(maptype) { 
+	var roadmapControl = document.getElementById('btnRoadmap');
+	var skyviewControl = document.getElementById('btnSkyview'); 
+	if (maptype === 'roadmap') {
+			map.setMapTypeId(kakao.maps.MapTypeId.ROADMAP);    
+			roadmapControl.className = 'selected_btn';
+			skyviewControl.className = 'btn';
+	} else {
+			map.setMapTypeId(kakao.maps.MapTypeId.HYBRID);    
+			skyviewControl.className = 'selected_btn';
+			roadmapControl.className = 'btn';
+	}
+}
+
+// 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+function zoomIn() {
+	map.setLevel(map.getLevel() - 1);
+}
+
+// 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+function zoomOut() {
+	map.setLevel(map.getLevel() + 1);
+}
